4036205a5b62fcf3163e36460e052f07b408cb32
Artiom Filipopovschii <artiom.filipopovschii@gmail.com>
2025-06-18 18:10:47 -0300

    feat: added CORS middleware for checking on frontend

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from app.api.v1 import posts, contact
+from fastapi.middleware.cors import CORSMiddleware
+
 import uvicorn
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4321"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(contact.router, prefix="/api/v1/contact", tags=["Contact"])
 app.include_router(posts.router, prefix="/api/v1/posts", tags=["Posts"])
 
