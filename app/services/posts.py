122dc6bd99cf44b18223c22cb80bd7b2f4031300
Artiom Filipopovschii <artiom.filipopovschii@gmail.com>
2025-06-19 19:18:32 -0300

    feat: created a many to many relationship database to link categories and posts

--- app/services/posts.py
+++ app/services/posts.py
@@ -1,17 +1,38 @@
-from app.schemas.posts import PostBase
-from app.models import Post
+from fastapi import HTTPException
+
+from app.schemas.posts import PostBase, PostCreate
+from app.models import Post, Category
 from sqlalchemy.orm import Session
 
 
 def get_posts(db: Session):
     return db.query(Post).all()
 
-def create_post(db: Session, data: PostBase):
-    post_instance = Post(**data.model_dump())
+def create_post(db: Session, data: PostCreate):
+    category_ids = data.category_ids
+
+    categories = db.query(Category).filter(Category.id.in_(category_ids)).all()
+
+    if len(categories) != len(set(category_ids)):
+        raise HTTPException(status_code=400, detail="One or more categories not found")
+
+    post_data = data.model_dump()
+    post_data.pop("category_ids", None)
+
+    post_instance = Post(
+        title=post_data.get("title"),
+        description=post_data.get("description"),
+        content=post_data.get("content"),
+    )
+
+    post_instance.categories = categories
+
     db.add(post_instance)
     db.commit()
     db.refresh(post_instance)
+
     return post_instance
+
 
 def get_post(db: Session, id: int):
     return db.query(Post).filter(Post.id == id).first()
