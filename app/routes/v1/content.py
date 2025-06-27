d7e83e9c6443a2bf799f6438712f5e50997bdb19
Artiom Filipopovschii <artiom.filipopovschii@gmail.com>
2025-06-27 17:12:18 -0300

    fix: change route for get content by post id

--- app/routes/v1/content.py
+++ app/routes/v1/content.py
@@ -11,7 +11,7 @@ from app.services.content import create_content_service, get_content_service, ge
 
 router = APIRouter()
 
-@router.post("/{post_id}/content/", response_model=PostContentOut, tags=["Content"], openapi_extra={
+@router.post("/{post_id}", response_model=PostContentOut, tags=["Content"], openapi_extra={
     "requestBody": {
         "content": {
             "application/json": {
@@ -92,22 +92,22 @@ def create_content_for_post(
 def get_content_for_post(post_id: int, db: Session = Depends(get_db)):
     return get_content_service(db, post_id)
 
-@router.get("/{post_id}/{content_id}/content", response_model=PostContentOut, tags=["Content"],
+@router.get("/{post_id}/{content_id}", response_model=PostContentOut, tags=["Content"],
             summary="Get content by ID")
 def get_content_by_content_id(post_id: int, content_id: int, db: Session = Depends(get_db)):
     return get_content_one_service(db, content_id)
 
-@router.delete("/{post_id}/{content_id}/content/", response_model=PostContentOut, tags=["Content"],
+@router.delete("/{post_id}/{content_id}", response_model=PostContentOut, tags=["Content"],
                summary="Delete a content by ID content")
 def delete_content_for_post(content_id: int, post_id: int, db: Session = Depends(get_db)):
     return delete_content_service(db, content_id)
 
-@router.delete('/{post_id}/content/', response_model=MessageResponse, tags=["Content"],
+@router.delete('/{post_id}', response_model=MessageResponse, tags=["Content"],
                summary="Delete a content by ID")
 def delete_all_content_for_post(post_id: int, db: Session = Depends(get_db)):
     return delete_all_post_content_service(db, post_id)
 
-@router.patch("/{post_id}/{content_id}/content/", response_model=PostContentOut, tags=["Content"],
+@router.patch("/{post_id}/{content_id}", response_model=PostContentOut, tags=["Content"],
               summary="Update a content by ID content")
 def update_content_for_post(content_id: int, data: PostContentUpdate, post_id: int, db: Session = Depends(get_db)):
     return update_content_service(db, content_id, data)
